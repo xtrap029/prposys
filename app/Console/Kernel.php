5de59d86471514c7f7e630bfd432e4ae21d7d896
kelvin <kmbarsana@gmail.com>
2021-03-19 18:16:56 +0800

    register_argc_argv

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->exec('php -d register_argc_argv=On /home/rbjorn2017/public_html/sequencing2/index database:backup');
+        $schedule->exec('php -d register_argc_argv=On /home/rbjorn2017/public_html/sequencing2/index database:backup')->everyMinute();
         // $schedule->command('database:backup')->everyMinute();
     }
 
