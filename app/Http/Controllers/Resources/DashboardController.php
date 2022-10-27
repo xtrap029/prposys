3d059740762759fa2b12acafcccfc1b7dfb303f4
Kelvin Barsana <kelvin.barsana@hoki.co>
2022-10-27 14:03:42 +0800

    resources dashboard

--- app/Http/Controllers/Resources/DashboardController.php
+++ app/Http/Controllers/Resources/DashboardController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Resources;
 use Illuminate\Http\Request;
 use Spatie\Activitylog\Models\Activity;
 use App\Http\Controllers\Controller;
+use App\Faq;
+use App\Form;
 use App\User;
 use App\Settings;
 use Carbon\Carbon;
@@ -14,9 +16,15 @@ class DashboardController extends Controller {
     public function index() {
         $user = User::where('id', auth()->id())->first();
         $announcement = Settings::where('type', 'ANNOUNCEMENT')->first()->value;
+        $forms = Form::orderBy('updated_at', 'desc')->limit(10)->get();
+        $faqs_updated = Faq::orderBy('updated_at', 'desc')->limit(5)->get();
+        $faqs_recent = Faq::orderBy('created_at', 'desc')->limit(5)->get();
         
         return view('pages.resources.dashboard.index')->with([
             'user' => $user,
+            'forms' => $forms,
+            'faqs_updated' => $faqs_updated,
+            'faqs_recent' => $faqs_recent,
             'announcement' => $announcement,
         ]);
     }
--- resources/views/pages/resources/dashboard/index.blade.php
+++ resources/views/pages/resources/dashboard/index.blade.php
@@ -42,6 +42,131 @@
             <div class="col-md-6 col-lg-8 d-flex">
                 @include('layouts.sections.slick')
             </div>
+            <div class="col-12">
+                <p>
+                    <a href="/forms" class="btn btn-dark rounded-pill p-4 mr-1" style="line-height: 15px;">
+                        Forms
+                    </a>
+                    @foreach ($forms as $item)
+                        <button type="button" class="btn btn-default rounded-pill p-3 mr-1" data-toggle="modal" data-target="#modal-{{ $item->id }}" style="line-height: 15px;">
+                            {{ $item->name }}
+                            <br><code>{{ $item->category }}</code>
+                        </button>
+                    @endforeach
+
+                    @foreach ($forms as $item)
+                        <div class="modal fade" id="modal-{{ $item->id }}" tabindex="-1" role="dialog" aria-hidden="true">
+                            <div class="modal-dialog modal-md" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header border-0">
+                                        <h5 class="modal-title">{{ $item->name }}</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        {!! $item->description !!}
+
+                                        <a class="btn btn-default d-block mt-3" href="/storage/public/attachments/form/{{ $item->attachment }}" target="_blank">
+                                            <i class="align-middle font-weight-bolder material-icons text-orange">folder</i>
+                                            <span class="text-dark">Download Attachment</span>
+                                        </a>
+                                    </div>
+                                    <div class="modal-footer border-0">
+                                        <i class="small">Last Updated: {{ $item->updated_at }}</i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </p>
+            </div>
+            <div class="col-md-6">
+                <div class="card card-widget">
+                    <div class="card-header pb-2 bg-dark">
+                        <h3 class="card-title">
+                            Newest FAQs
+                        </h3>
+                    </div>
+                    <div class="card-body p-0">
+                        <table class="table m-0">
+                            @foreach ($faqs_recent as $item)
+                                <tr>
+                                    <td>
+                                        <a href="#_" data-toggle="modal" data-target="#modal-{{ $item->id }}">{{ $item->title }}</a>
+                                        <br><code>{{ $item->category }}</code>
+                                    </td>
+                                    <td class="text-right">{{ Carbon::parse($item->created_at)->diffInDays(Carbon::now()) >= 1 ? $item->created_at->format('Y-m-d') : $item->created_at->diffForHumans() }}</td>
+                                </tr>
+                            @endforeach
+                        </table>
+
+                        @foreach ($faqs_recent as $item)
+                            <div class="modal fade" id="modal-{{ $item->id }}" tabindex="-1" role="dialog" aria-hidden="true">
+                                <div class="modal-dialog modal-md" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header border-0">
+                                            <h5 class="modal-title">{{ $item->title }}</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            {!! $item->description !!}
+                                        </div>
+                                        <div class="modal-footer border-0">
+                                            <i class="small">Last Updated: {{ $item->updated_at }}</i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>                
+            </div>
+            <div class="col-md-6">
+                <div class="card card-widget">
+                    <div class="card-header pb-2 bg-dark">
+                        <h3 class="card-title">
+                            Updated FAQs
+                        </h3>
+                    </div>
+                    <div class="card-body p-0">
+                        <table class="table m-0">
+                            @foreach ($faqs_updated as $item)
+                                <tr>
+                                    <td>
+                                        <a href="#_" data-toggle="modal" data-target="#modal-{{ $item->id }}">{{ $item->title }}</a>
+                                        <br><code>{{ $item->category }}</code>
+                                    </td>
+                                    <td class="text-right">{{ Carbon::parse($item->updated_at)->diffInDays(Carbon::now()) >= 1 ? $item->updated_at->format('Y-m-d') : $item->updated_at->diffForHumans() }}</td>
+                                </tr>
+                            @endforeach
+                        </table>
+
+                        @foreach ($faqs_updated as $item)
+                            <div class="modal fade" id="modal-{{ $item->id }}" tabindex="-1" role="dialog" aria-hidden="true">
+                                <div class="modal-dialog modal-md" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header border-0">
+                                            <h5 class="modal-title">{{ $item->title }}</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            {!! $item->description !!}
+                                        </div>
+                                        <div class="modal-footer border-0">
+                                            <i class="small">Last Updated: {{ $item->updated_at }}</i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>                
+            </div>
         </div>
     </div>
 </section>
